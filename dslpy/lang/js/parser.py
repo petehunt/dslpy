f6f438bf4e36bcec6f129f3b03359c372ca222d3
Pete Hunt <floydophone@gmail.com>
2012-09-13 23:31:46 -0400

    Fix bug

--- dslpy/lang/js/parser.py
+++ dslpy/lang/js/parser.py
@@ -8,10 +8,7 @@ class JSMacro(object):
     @staticmethod
     def evaluate(env, code):
         def _sub(match):
-            try:
-                return json.dumps(env[match.group("id")])
-            except KeyError:
-                raise KeyError, "Invalid token: %s" % match.group("id")
+            return json.dumps(eval(match.group("id"), env))
         return macros.RE_INLINE.sub(_sub, code)
-        
+
 macros.MacroManager.get_instance().register(JSMacro)
